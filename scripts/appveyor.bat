d32bed755033b05ae6c2e7b24cffa2396d2450d3
K.Takata <kentkt@csc.jp>
2017-06-25 12:33:46 +0900

    Skip tagged commit only on non-scheduled builds

--- scripts/appveyor.bat
+++ scripts/appveyor.bat
@@ -37,11 +37,13 @@ set CHERE_INVOKING=yes
 @git config user.email "%DEPLOY_USER_EMAIL%"
 git remote set-url --push origin "git@github.com:%APPVEYOR_REPO_NAME%.git"
 
-@rem Skip if the commit is tagged.
-git describe --tags --exact-match > NUL 2>&1
-if not ERRORLEVEL 1 (
-  @echo Build skipped.
-  goto end_update
+if not "%APPVEYOR_SCHEDULED_BUILD%"=="True" (
+  @rem Skip if the commit is tagged.
+  git describe --tags --exact-match > NUL 2>&1
+  if not ERRORLEVEL 1 (
+    @echo Build skipped.
+    goto end_update
+  )
 )
 
 set MSYSTEM=MSYS
